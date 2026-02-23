d7d5c4e9521ad2b8e6e52e910e28fedf74ba3eec
Aiden Adzich <aiden.adzich@itas.ca>
2026-02-23 13:20:43 -0800

    Ran biome

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,24 +1,24 @@
-import pluginSecurity from "eslint-plugin-security"; 
+import pluginSecurity from "eslint-plugin-security";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import globals from "globals";
 
 export default [
-    pluginSecurity.configs.recommended,
+	pluginSecurity.configs.recommended,
 
-    {
-        files: ["**/*.{js,mjs,cjs}"],
-        plugins: {
-            "simple-import-sort": simpleImportSort,
-        },
-        languageOptions: {
-            globals: {
-                ...globals.node,
-                ...globals.jest,
-            },
-        },
-        rules: {
-            "simple-import-sort/imports": "error",
-            "simple-import-sort/exports": "error",
-        },
-    },
+	{
+		files: ["**/*.{js,mjs,cjs}"],
+		plugins: {
+			"simple-import-sort": simpleImportSort,
+		},
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.jest,
+			},
+		},
+		rules: {
+			"simple-import-sort/imports": "error",
+			"simple-import-sort/exports": "error",
+		},
+	},
 ];
